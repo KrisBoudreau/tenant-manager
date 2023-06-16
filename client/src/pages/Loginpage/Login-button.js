3b799204ec741f0ea1ca35ece9478874d51a1fc9
KrisBoudreau <83515238+KrisBoudreau@users.noreply.github.com>
2023-06-16 10:30:23 -0400

    button

--- client/src/pages/Loginpage/Login-button.js
+++ client/src/pages/Loginpage/Login-button.js
@@ -5,7 +5,7 @@ import { Button } from "@mui/material";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+  return <Button sx = {{margin: 4}} onClick={() => loginWithRedirect()}>Log In</Button>;
 };
 
 export default LoginButton;
